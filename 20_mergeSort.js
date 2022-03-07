17c0a4e134c5054eadeb1c6c86ebc8f0c1fada92
YOON <50129701+yoondiver@users.noreply.github.com>
2022-03-07 23:53:36 +0900

    mergeSort

--- 20_mergeSort.js
+++ 20_mergeSort.js
@@ -1,1 +1,31 @@
-const mergeSort = function () {};
+const mergeSort = function (arr) {
+  // TODO: 여기에 코드를 작성합니다.
+  if (arr.length === 1) {
+    return arr; // 배열이 1이면 반환
+  }
+  let half = Math.floor(arr.length / 2);
+
+  // 배열 나눈거1
+  let one = mergeSort(arr.slice(0, half));
+
+  // 다른 배열1
+  let two = mergeSort(arr.slice(half));
+
+  // 병합한 배열 리턴
+  return merge(one, two);
+};
+
+const merge = (one, two) => {
+  let result = [];
+  while (one.length && two.length) {
+    if (one[0] <= two[0]) {
+      result.push(one.shift());
+    } else {
+      result.push(two.shift());
+    }
+  }
+
+  while (one.length) result.push(one.shift());
+  while (two.length) result.push(two.shift());
+  return result;
+};
