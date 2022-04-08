72005da59f17090b12bd20e6adae6cf814af330a
YOON <50129701+yoondiver@users.noreply.github.com>
2022-04-08 22:55:01 +0900

    robotPath

--- 25_robotPath.js
+++ 25_robotPath.js
@@ -16,20 +16,20 @@ const robotPath = function (room, src, dst) {
   const ny = room.length - 1;
   const nx = room[0].length - 1;
 
-  while (queue.length !== 0) {
-    let result = queue.shift();
-    let [y, x, cnt] = result;
+  // while(queue.length !== 0){
+  //   let result = queue.shift();
+  //   let [y, x, cnt] = result;
 
-    for (let i = 0; i < 4; i++) {
-      let dy = y + direction[i][0];
-      let dx = x + direction[i][1];
+  //   for(let i = 0; i < 4; i++){
+  //     let dy = y + direction[i][0];
+  //     let dx = x + direction[i][1];
 
-      if (dx < 0 || dy < 0 || dx > nx || dy > ny) continue;
-      if (room[dy][dx] === 1) continue;
-      if (visit[dy][dx]) continue;
-      visit[dy][dx] = 1;
-      queue.push([dy, dx, cnt + 1]);
-      if (dy === dst[0] && dx === dst[1]) return result[2];
-    }
-  }
+  //     if(dx < 0 || dy < 0 || dx > nx || dy > ny) continue;
+  //     if(room[dy][dx] === 1) continue;
+  //     if(visit[dy][dx]) continue
+  //     visit[dy][dx] = 1;
+  //     queue.push([dy, dx, cnt + 1]);
+  //     if(dy === dst[0] && dx === dst[1]) return result[2];
+  //   }
+  // }
 };
