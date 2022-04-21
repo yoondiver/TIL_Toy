4de5842f66e075da5874b07472acf53c16427c5b
YOON <50129701+yoondiver@users.noreply.github.com>
2022-04-21 22:09:13 +0900

    LCS

--- 34_LCS.js
+++ 34_LCS.js
@@ -2,4 +2,12 @@ const LCS = function (str1, str2) {
   const M = str1.length;
   const N = str2.length;
   const memo = [];
+
+  // 중복 계산을 방지하기 위해 왼, 오
+  for (let i = 0; i < M + 1; i++) memo.push(Array(N + 1).fill(-1));
+
+  const compareOneByOne = (left, right) => {
+    if (memo[left][right] !== -1) return memo[left][right];
+  };
+  return compareOneByOne;
 };
