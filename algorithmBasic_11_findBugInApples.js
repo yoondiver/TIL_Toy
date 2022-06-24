0daefef336be434f84d074c4e6c757e95e062ea7
YOON <50129701+yoondiver@users.noreply.github.com>
2022-06-24 23:40:31 +0900

    ab11

--- algorithmBasic_11_findBugInApples.js
+++ algorithmBasic_11_findBugInApples.js
@@ -1,10 +1,26 @@
-function findBugInApples(arr) {
+function removeExtremes(arr) {
   // TODO: 여기에 코드를 작성합니다.
+
+  let shortestLen = 20;
+  let longestLen = 0;
+  let shortestIdx = 0;
+  let longestIdx = 0;
+
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] === "B") {
-        return [i, j];
-      }
+    if (arr[i].length >= longestLen) {
+      longestLen = arr[i].length;
+      longestIdx = i;
+    }
+    if (arr[i].length <= shortestLen) {
+      shortestLen = arr[i].length;
+      shortestIdx = i;
     }
   }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== shortestIdx && i !== longestIdx) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
