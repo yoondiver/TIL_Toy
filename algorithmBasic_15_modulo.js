de46e4a85bd8c077fc549fa43300c8ab99df2905
YOON <50129701+yoondiver@users.noreply.github.com>
2022-07-12 11:54:36 +0900

    ab15

--- algorithmBasic_15_modulo.js
+++ algorithmBasic_15_modulo.js
@@ -1,3 +1,13 @@
 function modulo(num1, num2) {
   // TODO: 여기에 코드를 작성합니다.
+  let first = num1;
+  let second = num2;
+
+  if (num1 === 0) return 0;
+  if (num2 === 0) return "Error: cannot divide by zero";
+
+  for (let i = second; i <= first; i += second) {
+    num1 = num1 - num2;
+  }
+  return num1;
 }
