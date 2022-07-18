963f232d097fa508271416f593675c26200c97fd
YOON <50129701+yoondiver@users.noreply.github.com>
2022-07-18 19:16:59 +0900

    ab16

--- algorithmBasic_16_isIsogram.js
+++ algorithmBasic_16_isIsogram.js
@@ -1,3 +1,18 @@
 function isIsogram(str) {
   // TODO: 여기에 코드를 작성합니다.
+
+  let trans = str.toLowerCase();
+  // let comp = trans[0]
+  // if(str.length === 0) {
+  //   return true
+  // }
+
+  for (let i = 0; i < trans.length - 1; i++) {
+    for (let j = i + 1; j < trans.length; j++) {
+      if (trans[i] === trans[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
