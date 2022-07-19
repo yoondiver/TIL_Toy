0ba101b67cc61370ce6ee95d8107866cf8e9847f
YOON <50129701+yoondiver@users.noreply.github.com>
2022-07-19 20:38:55 +0900

    ab17

--- algorithmBasic_17_computeSquareRoot.js
+++ algorithmBasic_17_computeSquareRoot.js
@@ -1,3 +1,21 @@
 function computeSquareRoot(num) {
   // TODO: 여기에 코드를 작성합니다.
+
+  let guess = Math.floor(num / 2);
+  let n = 0; // while문 조건식
+  while (true) {
+    n++;
+    let divide = num / guess;
+    let decimalEight = Number(divide.toFixed(8));
+    let average = (decimalEight + guess) / 2; // 추측값이랑 나눈값 중간값구하기
+    let checkNum = Math.floor(average);
+    if (checkNum * checkNum === num) {
+      return checkNum;
+    } else if (average * average < num) {
+      guess = average;
+      break;
+    }
+    guess = average;
+  }
+  return Number(guess.toFixed(2));
 }
