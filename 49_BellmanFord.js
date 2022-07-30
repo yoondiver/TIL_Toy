439ec169d1b7e0ddcbfd21edaf2432e6d78b5f57
YOON <50129701+yoondiver@users.noreply.github.com>
2022-07-30 23:00:37 +0900

    toy 49

--- 49_BellmanFord.js
+++ 49_BellmanFord.js
@@ -1,3 +1,7 @@
+// 그래프 구현이 필요없는 알고리즘: O(V * E)
+// V는 정점의 개수, E는 간선의 개수
 function BellmanFord(num, edges, start) {
-  // TODO: 여기에 코드를 작성합니다.
+  const INF = Number.MAX_SAFE_INTEGER;
+  const dist = Array(num + 1).fill(INF);
+  dist[start] = 0;
 }
