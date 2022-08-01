c53e8a8c3f811f3aade668b74b61aa5d33e77fc5
YOON <50129701+yoondiver@users.noreply.github.com>
2022-08-01 19:17:40 +0900

    toy50

--- 50_findAbbreviation.js
+++ 50_findAbbreviation.js
@@ -1,3 +1,9 @@
 function findAbbreviation(strA, strB) {
   // TODO: 여기에 코드를 작성합니다.
+
+  const N = 100;
+  // 중복 계산을 제거하기 위한 memo 배열
+  // 계산되지 않은 상태를 -1로 초기화한다.
+  const memo = [];
+  for (let i = 0; i < N; i++) memo.push(Array(N).fill(-1));
 }
