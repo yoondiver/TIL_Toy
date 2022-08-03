77d48145c8322315bb7e60ac8b7905afcbf9b308
YOON <50129701+yoondiver@users.noreply.github.com>
2022-08-03 21:45:38 +0900

    toy50 line

--- 50_findAbbreviation.js
+++ 50_findAbbreviation.js
@@ -1,3 +1,44 @@
+// naive solution
+// function findAbbreviation(strA, strB) {
+//   const isLower = (chr) => chr.toUpperCase() !== chr;
+
+//   const aux = (leftIdx, rightIdx) => {
+//     // base case 1
+//     // strA로 strB를 만들 수 있고
+//     // strA의 문자가 남아있지 않거나, 남은 문자가 전부 소문자인 경우 (전부 제거 가능)
+//     if (rightIdx === strB.length)
+//       return strA.substring(leftIdx).split('').every(isLower);
+
+//     // base case 2
+//     // strA의 모든 문자를 검토했으나
+//     // strB의 문자가 남아있는 경우
+//     if (leftIdx === strA.length) return false;
+
+//     // recursive case
+//     if (isLower(strA[leftIdx])) {
+//       // 1) 현재 검토 중인 strA의 문자가 소문자인 경우
+//       // 대문자로 변경하거나 제거가 가능한다.
+//       // 매칭이 되지 않은 경우 제거한다.
+//       if (strA[leftIdx].toUpperCase() !== strB[rightIdx])
+//         return aux(leftIdx + 1, rightIdx);
+//       // 매칭이 된 경우, 문자로 변경하거나 제거한다.
+//       // 매칭이 되었는데도 제거를 하는 경우의 예시 (입력으로 'eE', 'E'를 받은 경우)
+//       return aux(leftIdx + 1, rightIdx + 1) || aux(leftIdx + 1, rightIdx);
+//     } else {
+//       // 2) 현재 검토 중인 strA의 문자가 대문자인 경우
+//       // 대문자는 제거가 불가능하고 그대로 사용할 수 밖에 없다.
+//       // 따라서 매칭 여부가 중요하다.
+//       // 매칭되지 않은 경우 탐색을 중단한다.
+//       if (strA[leftIdx] !== strB[rightIdx]) return false;
+//       // 매칭이 된 경우 다음 문자를 검토한다.
+//       return aux(leftIdx + 1, rightIdx + 1);
+//     }
+//   };
+
+//   return aux(0, 0);
+// }
+
+// memoization
 function findAbbreviation(strA, strB) {
   const N = 100;
   // 중복 계산을 제거하기 위한 memo 배열
