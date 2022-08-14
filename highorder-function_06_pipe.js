5b23223067d8fed821d0ec4a1ca669acf4ee697a
YOON <50129701+yoondiver@users.noreply.github.com>
2022-08-14 23:11:18 +0900

    hof06

--- highorder-function_06_pipe.js
+++ highorder-function_06_pipe.js
@@ -1,3 +1,10 @@
 function pipe(...funcs) {
   // TODO: 여기에 코드를 작성합니다.
+  return function (num) {
+    let result = num;
+    for (let i = 0; i < funcs.length; i++) {
+      result = funcs[i](result);
+    }
+    return result;
+  };
 }
