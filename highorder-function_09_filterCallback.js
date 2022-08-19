13c8b0d2dc860ae07c3b4100c387553b37abcf61
YOON <50129701+yoondiver@users.noreply.github.com>
2022-08-19 22:24:06 +0900

    hof09

--- highorder-function_09_filterCallback.js
+++ highorder-function_09_filterCallback.js
@@ -1,4 +1,10 @@
 function filterCallback(func, arr) {
   // TODO: 여기에 코드를 작성합니다.
   let result = [];
+  for (let el of arr) {
+    if (func(el)) {
+      result.push(el);
+    }
+  }
+  return result;
 }
