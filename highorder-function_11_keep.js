1fc8e497841b5acf2044a55ee117d406dd4499f8
YOON <50129701+yoondiver@users.noreply.github.com>
2022-08-23 23:31:50 +0900

    hof11

--- highorder-function_11_keep.js
+++ highorder-function_11_keep.js
@@ -1,4 +1,8 @@
 function keep(arr, keeper) {
   // TODO: 여기에 코드를 작성합니다.
-  return arr.filter(function (el) {});
+  return arr.filter(function (el) {
+    if (el === keeper) {
+      return el;
+    }
+  });
 }
