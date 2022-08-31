ca55f5d2d2731e5b377385a4ec8647c99133cf04
YOON <50129701+yoondiver@users.noreply.github.com>
2022-08-31 19:33:07 +0900

    hof14

--- highorder-function_14_.js
+++ highorder-function_14_.js
@@ -5,4 +5,10 @@ function lessThan100(number) {
 function getElementsLessThan100AtProperty(obj, property) {
   // TODO: 여기에 코드를 작성합니다.
   let valueIsArr = obj[property];
+  if (Array.isArray(valueIsArr)) {
+    let result = valueIsArr.filter(
+      (el) => typeof el === "number" && lessThan100(el)
+    );
+    return result;
+  } else return [];
 }
