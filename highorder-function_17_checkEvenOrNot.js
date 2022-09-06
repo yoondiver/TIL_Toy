408fbc8b711c8b31a1861ddedbe8ea932942a92f
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-06 22:13:16 +0900

    hof17

--- highorder-function_17_checkEvenOrNot.js
+++ highorder-function_17_checkEvenOrNot.js
@@ -1,4 +1,7 @@
 function checkEvenOrNot(arr) {
   // TODO: 여기에 코드를 작성합니다.
-  return arr.map(function (el) {});
+  return arr.map(function (el) {
+    if (el % 2 === 0 && el !== 0) return "ok";
+    return "no";
+  });
 }
