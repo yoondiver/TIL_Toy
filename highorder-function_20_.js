c53622e1a5b1c1cb6b6068edef3408a70db90293
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-08 23:00:30 +0900

    hof20

---
+++ highorder-function_20_.js
@@ -0,0 +1,13 @@
+function square(number) {
+  return number * number;
+}
+
+function getSquaredElementsAtProperty(obj, property) {
+  // TODO: 여기에 코드를 작성합니다.
+  let arr = obj[property];
+  if (Array.isArray(arr)) {
+    return arr.map((el) => square(el));
+  } else {
+    return [];
+  }
+}
