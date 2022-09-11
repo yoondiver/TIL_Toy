a9187556cbcd386a1de0986c1a384d28139ad97e
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-11 19:22:07 +0900

    hof22

--- highorder-function_22_.js
+++ highorder-function_22_.js
@@ -1,4 +1,6 @@
 function classicMovies(arr, year) {
   // TODO: 여기에 코드를 작성합니다.
-  const result = arr.filter(function (movie) {});
+  const result = arr.filter(function (movie) {
+    return movie.year < year;
+  });
 }
