d284a424568140979dd7d387669cf32766736045
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-18 22:06:32 +0900

    hof26

--- highorder-function_26_calculateScore.js
+++ highorder-function_26_calculateScore.js
@@ -1,4 +1,6 @@
 function calculateScore(records, value) {
   // TODO: Your code here!
-  let sum = records.reduce((acc, cur) => {}, 0);
+  let sum = records.reduce((acc, cur) => {
+    if (cur.animal === value) return acc + cur.score;
+  }, 0);
 }
