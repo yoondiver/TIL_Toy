315dc853c39b409af3e7588e06debe818ac25bcd
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-22 23:12:32 +0900

    hof28

--- highorder-function_28_getLengthOfLongestElement.js
+++ highorder-function_28_getLengthOfLongestElement.js
@@ -1,5 +1,7 @@
 function getLengthOfLongestElement(arr) {
   // 여기에 코드를 입력하세요
   if (arr.length === 0) return 0;
-  return arr.reduce((acc, cur) => {});
+  return arr.reduce((acc, cur) => {
+    if (cur.length <= acc.length) return acc; // <도 됨
+  });
 }
