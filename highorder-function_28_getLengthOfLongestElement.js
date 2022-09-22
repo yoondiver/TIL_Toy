ff2186d8b370a9fb91673e1244605cff2861851f
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-22 23:12:48 +0900

    hof28

--- highorder-function_28_getLengthOfLongestElement.js
+++ highorder-function_28_getLengthOfLongestElement.js
@@ -3,5 +3,6 @@ function getLengthOfLongestElement(arr) {
   if (arr.length === 0) return 0;
   return arr.reduce((acc, cur) => {
     if (cur.length <= acc.length) return acc; // <도 됨
-  });
+    else return cur;
+  }).length;
 }
