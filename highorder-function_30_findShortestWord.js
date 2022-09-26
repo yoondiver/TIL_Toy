822a7d486094324015d0d750852bd95c52dee018
YOON <50129701+yoondiver@users.noreply.github.com>
2022-09-27 00:03:15 +0900

    hof30

--- highorder-function_30_findShortestWord.js
+++ highorder-function_30_findShortestWord.js
@@ -1,4 +1,6 @@
 function findShortestWord(arr) {
   // TODO: 여기에 코드를 작성합니다.
   let strArr = arr.filter((el) => typeof el === "string");
+  if (strArr.length === 0) return "";
+  return strArr.reduce((acc, cur) => {});
 }
