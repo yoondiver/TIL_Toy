72dd54b2205ac5aeb31fe0902956dc04098c5e7d
YOON <50129701+yoondiver@users.noreply.github.com>
2022-10-03 21:53:58 +0900

    hof32

--- highorder-function_32_sumOfArraysInArray.js
+++ highorder-function_32_sumOfArraysInArray.js
@@ -2,7 +2,7 @@ function sumOfArraysInArray(arr) {
   // TODO: 여기에 코드를 작성합니다.
   let flatted = arr.flat();
   let numArr = flatted.filter((el) => {
-    return typeof el=== 'number';
+    return typeof el === "number";
   });
-  return numArr.reduce((acc, cur)=> {);
+  return numArr.reduce((acc, cur) => {});
 }
