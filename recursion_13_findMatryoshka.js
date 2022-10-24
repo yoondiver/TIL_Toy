7829071f4ecb9bd2801fef127e2647ea340cb51e
YOON <sungan1983@gmail.com>
2022-10-24 23:05:58 +0900

    re13

--- recursion_13_findMatryoshka.js
+++ recursion_13_findMatryoshka.js
@@ -2,7 +2,7 @@ function findMatryoshka(matryoshka, size) {
   // TODO: 여기에 코드를 작성합니다.
   if (matryoshka.size === size) {
     return true;
-  } else if (matryoshka) {
+  } else if (matryoshka.matryoshka) {
     return;
   }
 }
