b92e48686a508bb3dec8d6acd7bea64847c0fa1b
YOON <50129701+yoondiver@users.noreply.github.com>
2022-11-10 16:15:12 +0900

    ob13

--- object_13_printObject.js
+++ object_13_printObject.js
@@ -2,9 +2,8 @@ function printObject(obj) {
   // TODO: 여기에 코드를 작성합니다.
 
   let result = "";
-
   for (let i in obj) {
-    result = result;
+    result = result + `${i}: ${obj[i]}\n`;
   }
   return result;
 }
