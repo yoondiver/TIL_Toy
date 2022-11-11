a9f08e87ec0df97f122b9ec2c5ee22ce8a0d2aed
YOON <sungan1983@gmail.com>
2022-11-11 23:05:54 +0900

    ob14

--- object_14_getElementOfArrayProperty.js
+++ object_14_getElementOfArrayProperty.js
@@ -1,6 +1,10 @@
 function getElementOfArrayProperty(obj, key, index) {
   // TODO: 여기에 코드를 작성합니다.
   let arrProperty = obj[key];
+
   for (const key in obj) {
+    if (Array.isArray(obj[key]) === true) {
+      return;
+    }
   }
 }
