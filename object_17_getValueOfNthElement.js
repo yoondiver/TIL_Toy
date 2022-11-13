6d394ae7aab69ced099326825b8e348657e492e0
YOON <50129701+yoondiver@users.noreply.github.com>
2022-11-13 19:40:52 +0900

    ob17

--- object_17_getValueOfNthElement.js
+++ object_17_getValueOfNthElement.js
@@ -1,11 +1,10 @@
 function getValueOfNthElement(arr, num) {
   // TODO: 여기에 코드를 작성합니다.
   if (arr.length === 0) {
-    return arr[arr.length - 1];
+    return "no name";
   }
-
   if (arr.length <= num) {
-    return;
+    return arr[arr.length - 1]["name"];
   } else if (arr[num]) {
     return arr[num]["name"];
   }
