6db378178ffb02633a1c3e7fa5bd6f3b72519e4c
YOON <sungan1983@gmail.com>
2022-11-14 22:08:34 +0900

    ob18

--- object_18_.js
+++ object_18_.js
@@ -2,7 +2,7 @@ function getAllButLastElementOfProperty(obj, key) {
   // TODO: 여기에 코드를 작성합니다.
 
   let arr = [];
-  if (Array.isArray(obj[key] === true)) {
+  if (Array.isArray(obj[key]) === true && obj[key].length !== 0) {
     return;
   } else {
     return [];
