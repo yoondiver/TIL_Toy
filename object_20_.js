6eedac66f46de146948cf268839a115017c31b87
YOON <sungan1983@gmail.com>
2022-11-16 23:31:34 +0900

    ob20

--- object_20_.js
+++ object_20_.js
@@ -1,14 +1,3 @@
 function countAllCharacters(str) {
   // TODO: 여기에 코드를 작성합니다.
-
-  let result = {};
-
-  for (let char of str) {
-    if (char in result === false) {
-      result[char] = 1;
-    } else {
-      result[char] += 1;
-    }
-  }
-  return result;
 }
