3f66ace1fe5ce77f71929c80f2a452e5d4cead5f
YOON <sungan1983@gmail.com>
2022-11-27 05:14:32 +0900

    IF6

---
+++ if_06_isEvenAndGreaterThanTen.js
@@ -0,0 +1,10 @@
+function isEvenAndGreaterThanTen(num) {
+  // TODO: 여기에 코드를 작성합니다.
+  // num이 10보다 크고, 짝수일때 true, 아니면 false
+
+  if (num > 10 && num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
