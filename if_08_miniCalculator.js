85681ac2d0b9552a181462832e90590d496e258b
YOON <sungan1983@gmail.com>
2022-11-29 22:43:48 +0900

    if8

---
+++ if_08_miniCalculator.js
@@ -0,0 +1,23 @@
+function miniCalculator(num1, num2, operator) {
+  // TODO: 여기에 코드를 작성합니다.
+
+  // num1과 num2를 operator로 계산
+
+  // +
+  if (operator === "+") {
+    return num1 + num2;
+  }
+
+  // -
+  else if (operator === "-") {
+    return num1 - num2;
+  }
+
+  // *
+  else if (operator === "/") {
+    return num1 / num2;
+  }
+
+  // *
+  else return num1 * num2;
+}
