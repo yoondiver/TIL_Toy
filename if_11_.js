19cfe6a1ba736d1f50c8e3c6b52c1852ef83d7b4
YOON <sungan1983@gmail.com>
2022-12-01 20:20:14 +0900

    if11

--- if_11_.js
+++ if_11_.js
@@ -1,3 +1,6 @@
 function isEitherEvenAndLessThan9(num1, num2) {
   // TODO: 여기에 코드를 작성합니다.
+  // if((num1 %2 === 1 && num2 %2 === 1) || (num1 >=9 || num2 >=9)){
+  //   return false
+  // } else return true
 }
