5c2af12401685d26df7324a14e5e72c29a1fe074
YOON <sungan1983@gmail.com>
2022-12-01 20:20:28 +0900

    if11

--- if_11_.js
+++ if_11_.js
@@ -3,4 +3,8 @@ function isEitherEvenAndLessThan9(num1, num2) {
   // if((num1 %2 === 1 && num2 %2 === 1) || (num1 >=9 || num2 >=9)){
   //   return false
   // } else return true
+
+  if (num1 < 9 && num2 < 9 && (num1 % 2 === 0 || num2 % 2 === 0)) {
+    return true;
+  }
 }
