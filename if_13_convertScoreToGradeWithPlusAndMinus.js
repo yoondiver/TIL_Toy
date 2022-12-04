9e70fb8bb393ad69af791393a0ab4b33cbe16715
YOON <50129701+yoondiver@users.noreply.github.com>
2022-12-04 20:59:02 +0900

    if13

--- if_13_convertScoreToGradeWithPlusAndMinus.js
+++ if_13_convertScoreToGradeWithPlusAndMinus.js
@@ -2,5 +2,7 @@ function convertScoreToGradeWithPlusAndMinus(score) {
   // TODO: 여기에 코드를 작성합니다.
   if (score > 100 || score < 0) {
     return "INVALID SCORE";
+  } else if (score > 98) {
+    return "A+";
   }
 }
