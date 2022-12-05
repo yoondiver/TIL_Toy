8563dfc9e342479e077402bdbeef19065e2203dc
YOON <sungan1983@gmail.com>
2022-12-06 01:57:47 +0900

    if14

--- if_14_.js
+++ if_14_.js
@@ -1,6 +1,6 @@
 function isPythagorean(side1, side2, side3) {
   // TODO: 여기에 코드를 작성합니다.
-
+  //
   if (side1 > side2 && side1 > side3) {
     if (Math.pow(side1, 2) === side2 * side2 + Math.pow(side3, 2)) {
       return true;
