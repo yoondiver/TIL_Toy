752f4c914c315d6547ebf8c82255eeb2c338e129
YOON <sungan1983@gmail.com>
2022-12-06 01:57:04 +0900

    if14

--- if_14_.js
+++ if_14_.js
@@ -9,5 +9,9 @@ function isPythagorean(side1, side2, side3) {
     if (Math.pow(side2, 2) === side1 ** 2 + Math.pow(side3, 2)) {
       return true;
     } else return false;
+  } else if (side3 > side1 && side3 > side2) {
+    if (Math.pow(side3, 2) === side2 * side2 + Math.pow(side1, 2)) {
+      return true;
+    } else return false;
   }
 }
