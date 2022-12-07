497e45ec8733c295084483cbd0bead4e07d2142e
YOON <sungan1983@gmail.com>
2022-12-07 23:08:49 +0900

    if16

--- if_16_.js
+++ if_16_.js
@@ -1,3 +1,8 @@
 function addOneSecond(hour, minute, second) {
   // TODO: 여기에 코드를 작성합니다.
+
+  if (hour !== 23 && minute !== 59 && second !== 59) {
+    second = second + 1;
+    return "1초 뒤에 " + hour + "시 " + minute + "분 " + second + "초 입니다";
+  }
 }
