bbb5442507e4225c720dd3eaa31c63eb6da126f3
YOON <sungan1983@gmail.com>
2022-12-07 23:08:59 +0900

    if16

--- if_16_.js
+++ if_16_.js
@@ -4,5 +4,8 @@ function addOneSecond(hour, minute, second) {
   if (hour !== 23 && minute !== 59 && second !== 59) {
     second = second + 1;
     return "1초 뒤에 " + hour + "시 " + minute + "분 " + second + "초 입니다";
+  } else if ((hour !== 23) & (minute !== 59) && second === 59) {
+    minute = minute + 1;
+    return "1초 뒤에 " + hour + "시 " + minute + "분 0초 입니다";
   }
 }
