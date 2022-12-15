34a11a514252698aaf62ac51a962e98728df2f47
YOON <50129701+yoondiver@users.noreply.github.com>
2022-12-15 18:41:51 +0900

    s6

--- string_06_.js
+++ string_06_.js
@@ -1,3 +1,7 @@
 function findShortestOfThreeWords(word1, word2, word3) {
   // TODO: 여기에 코드를 작성합니다.
+
+  if (word1.length <= word2.length && word1.length <= word3.length) {
+    return word1;
+  }
 }
