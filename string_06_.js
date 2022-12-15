6d0985c0a8c10a5ef74aceccec55ee08f737c36b
YOON <50129701+yoondiver@users.noreply.github.com>
2022-12-15 18:42:16 +0900

    s6

--- string_06_.js
+++ string_06_.js
@@ -7,4 +7,7 @@ function findShortestOfThreeWords(word1, word2, word3) {
   if (word2.length <= word1.length && word2.length <= word3.length) {
     return word2;
   }
+  if (word3.length <= word2.length && word3.length <= word1.length) {
+    return word3;
+  }
 }
