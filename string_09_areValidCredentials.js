e21e85025692df7afe7f4866a2f8ff8d948abc97
YOON <50129701+yoondiver@users.noreply.github.com>
2022-12-18 18:05:07 +0900

    s9

--- string_09_areValidCredentials.js
+++ string_09_areValidCredentials.js
@@ -1,7 +1,3 @@
-function areValidCredentials(name, password) {
+function findMinLengthOfThreeWords(word1, word2, word3) {
   // TODO: 여기에 코드를 작성합니다.
-
-  if (name.length > 3 && password.length > 8) {
-    return true;
-  } else return false;
 }
