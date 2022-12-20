7c5ec635511f521f3596fd72b2c19e19da8e260d
YOON <sungan1983@gmail.com>
2022-12-20 23:10:14 +0900

    s10

--- string_10_.js
+++
@@ -1,9 +0,0 @@
-function takeLetters(num, str) {
-  // TODO: 여기에 코드를 작성합니다.
-  // num 개수만큼 스트링 리턴
-  // str num 비교해서 작은수 리턴
-
-  if (num <= str.length) {
-    return str.slice(0, num);
-  } else return str.slice(0, str.length);
-}
