6cef2fbf090d3a451f384aaa4d411844c6970aee
YOON <50129701+yoondiver@users.noreply.github.com>
2022-12-23 16:18:52 +0900

    s13

--- string_13_.js
+++ string_13_.js
@@ -1,6 +1,9 @@
 function compareNumbers(num1, num2) {
   // TODO: 여기에 코드를 작성합니다.
-  //num1 이 클경우
-  //num2 이 클경우
+  // num1 이 클경우
+  if (num1 > num2) {
+    return `${num1}은(는) ${num2}보다 큽니다`;
+  }
+  // num2 이 클경우
   // 같을경우
 }
