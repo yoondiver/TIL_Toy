522064a7c13dccad984e9426c9c6e0cadc883966
YOON <sungan1983@gmail.com>
2022-12-25 14:22:35 +0900

    s14

---
+++ string_14_compareNumberStrings.js
@@ -0,0 +1,7 @@
+function compareNumberStrings(numStr1, numStr2) {
+  // TODO: 여기에 코드를 작성합니다.
+
+  if (numStr1 > numStr2) {
+    return String(Number(numStr1) + 5);
+  } else return String(Number(numStr2) + 5);
+}
