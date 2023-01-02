93698ddddf2c262a3abcaffaebe9c851da38f7dc
YOON <sungan1983@gmail.com>
2023-01-02 22:46:10 +0900

    p0-2

--- programmers_lv0_02.js
+++ programmers_lv0_02.js
@@ -1,8 +1,3 @@
-function solution(num1, num2) {
-  let answer = parseInt(num1 / num2);
-  return answer;
-}
-
 // 프로그래머스 - 몫 구하기
 // 문제 설명
 // 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
@@ -13,3 +8,8 @@ num1이 10, num2가 5이므로 10을 5로 나눈 몫 2를 return 합니다.
 입출력 예 #2
 num1이 7, num2가 2이므로 7을 2로 나눈 몫 3을 return 합니다.
 */
+
+function solution(num1, num2) {
+  let answer = parseInt(num1 / num2);
+  return answer;
+}
