272b90705e1b02efb96252f7f3af2930249fb974
YOON <sungan1983@gmail.com>
2023-01-03 22:19:54 +0900

    p0-7

--- programmers_lv0_07.js
+++ programmers_lv0_07.js
@@ -1,6 +1,6 @@
-// 연속된 수의 합
+// 숫자 비교하기
 /**
- * 연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다. 연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+ * 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
  */
 
 function solution(num, total) {
