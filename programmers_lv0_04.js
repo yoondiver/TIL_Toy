4e15a2bdb94acd1ed8c2a33d70494001bcc91287
YOON <sungan1983@gmail.com>
2023-01-11 14:34:57 +0900

    p0-4

--- programmers_lv0_04.js
+++
@@ -1,9 +0,0 @@
-// 나머지 구하기
-/**
- * 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
- */
-
-function solution(num1, num2) {
-  let answer = num1 % num2;
-  return answer;
-}
