d80937f65880dc4f88315e01bbe29368f4b961e2
YOON <sungan1983@gmail.com>
2023-01-14 14:55:10 +0900

    p0-15

--- programmers_lv0_15_머쓱이.js
+++ programmers_lv0_15_머쓱이.js
@@ -6,6 +6,11 @@
  */
 
 function solution(array, height) {
-  var answer = 0;
+  let answer = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > height) {
+      answer += 1;
+    }
+  }
   return answer;
 }
