29afc3ab2e2a4a52992a71e25e802686ccfa0da9
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-15 22:34:34 +0900

    p0-16

---
+++ programmers_lv0_16_분수.js
@@ -0,0 +1,10 @@
+// 분수의 덧셈
+/**
+ * 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+ * 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+ */
+
+function solution(numer1, denom1, numer2, denom2) {
+  var answer = [];
+  return answer;
+}
