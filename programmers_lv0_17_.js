8d051dbd85d2219b96010c9f5bace972662735f3
YOON <sungan1983@gmail.com>
2023-01-16 23:04:23 +0900

    p0-17

--- programmers_lv0_17_.js
+++ programmers_lv0_17_.js
@@ -8,5 +8,12 @@ function solution(array, n) {
   //리턴값으로 쓸 정수
   var answer = 0;
 
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == n) {
+      //array의 i번째수와 매개변수 n이 같다면
+      answer++; //answer 1 증가
+    }
+  }
+
   return answer;
 }
