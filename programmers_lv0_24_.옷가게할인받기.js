e39052d72f220d97e998846172c92972cb523c67
YOON <sungan1983@gmail.com>
2023-01-20 17:37:35 +0900

    p0-24

--- programmers_lv0_24_.옷가게할인받기.js
+++ programmers_lv0_24_.옷가게할인받기.js
@@ -8,6 +8,14 @@
  */
 
 function solution(price) {
-  var answer = 0;
+  var answer = price;
+
+  if (price >= 500000) {
+    answer = parseInt(price * 0.8);
+  } else if (price >= 300000 && price < 500000) {
+    answer = parseInt(price * 0.9);
+  } else if (price >= 100000 && price < 300000) {
+    answer = parseInt(price * 0.95);
+  }
   return answer;
 }
