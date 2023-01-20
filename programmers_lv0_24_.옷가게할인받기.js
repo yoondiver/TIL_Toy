9bebac765a86a634f3228b4b86ad106ef6b9733b
YOON <sungan1983@gmail.com>
2023-01-20 17:37:00 +0900

    p0-24

---
+++ programmers_lv0_24_.옷가게할인받기.js
@@ -0,0 +1,13 @@
+// 옷가게할인받기
+
+/**
+ * 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%,
+ * 50만 원 이상 사면 20%를 할인해줍니다.
+ * 구매한 옷의 가격 price가 주어질 때,
+ * 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+ */
+
+function solution(price) {
+  var answer = 0;
+  return answer;
+}
