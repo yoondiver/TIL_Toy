b8197563bf565b12741a74c41a4e4b1ddd1e08ed
YOON <sungan1983@gmail.com>
2023-01-22 01:35:28 +0900

    ㅔ0-26

---
+++ programmers_lv0_26_문자열뒤집기.js
@@ -0,0 +1,11 @@
+// 문자열 뒤집기
+
+/**
+ * 문자열 my_string이 매개변수로 주어집니다.
+ * my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(my_string) {
+  var answer = "";
+  return answer;
+}
