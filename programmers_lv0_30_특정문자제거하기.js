e25a38e68911a2c9a231aad347cd82bbb04feba4
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-22 22:58:00 +0900

    p0-30

---
+++ programmers_lv0_30_특정문자제거하기.js
@@ -0,0 +1,10 @@
+// 특정 문자 제거하기
+/**
+ * 문자열 my_string과 문자 letter이 매개변수로 주어집니다.
+ * my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(my_string, letter) {
+  var answer = "";
+  return answer;
+}
