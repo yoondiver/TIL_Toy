5403e998bb4859d9fcd79df1f3528a64ed8d3c5b
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-22 22:51:05 +0900

    p0-28

--- programmers_lv0_28_짝수홀수개수.js
+++ programmers_lv0_28_짝수홀수개수.js
@@ -7,5 +7,17 @@
 
 function solution(num_list) {
   var answer = [];
+
+  let sum1 = 0;
+  let sum2 = 0;
+
+  for (let i = 0; i < num_list.length; i++) {
+    if (num_list[i] % 2 == 0) {
+      sum1++;
+    } else {
+      sum2++;
+    }
+  }
+  answer.push(sum1, sum2);
   return answer;
 }
