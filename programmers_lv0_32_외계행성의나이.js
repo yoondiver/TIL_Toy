fb4ebb0726748a65077ff4bd2c162f389cc239b2
YOON <sungan1983@gmail.com>
2023-01-23 14:01:35 +0900

    p0-32

--- programmers_lv0_32_외계행성의나이.js
+++ programmers_lv0_32_외계행성의나이.js
@@ -9,6 +9,23 @@
  */
 
 function solution(age) {
-  var answer = "";
-  return answer;
+  let newAge = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+    9: "j",
+  };
+
+  let result = age + "";
+
+  return result
+    .split("")
+    .map((el) => newAge[el])
+    .join("");
 }
