07f35becb2f0cf801977aa72bd42042ce43586b0
YOON <sungan1983@gmail.com>
2023-01-23 14:11:55 +0900

    p0-34

--- programmers_lv0_34_순서쌍의개수.js
+++ programmers_lv0_34_순서쌍의개수.js
@@ -14,3 +14,14 @@ function solution(n) {
   }
   return result.length;
 }
+
+/**
+ * 
+ * function solution(n) {
+    let ans = 0;
+    for (let i = 1; i < Math.sqrt(n); i++)
+        if (n%i === 0) ans+=2;
+
+    return Number.isInteger(Math.sqrt(n)) ? ans+1 : ans;
+}
+ */
