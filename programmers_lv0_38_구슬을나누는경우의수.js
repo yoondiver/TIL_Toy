5ff2b3f9d8d31c6f5f0835dc7e9c6d4bb2154d48
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-24 22:22:30 +0900

    p0-38

---
+++ programmers_lv0_38_구슬을나누는경우의수.js
@@ -0,0 +1,12 @@
+// 구슬을 나누는 경우의 수
+/**
+ * 머쓱이는 구슬을 친구들에게 나누어주려고 합니다.
+ * 구슬은 모두 다르게 생겼습니다.
+ * 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때,
+ * balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+ */
+
+function solution(balls, share) {
+  var answer = 0;
+  return answer;
+}
