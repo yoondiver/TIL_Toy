39456af8db7e822d9468047cdb0cfd1f964cfc84
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-24 22:19:18 +0900

    p0-37

--- programmers_lv0_37_가위바위보.js
+++ programmers_lv0_37_가위바위보.js
@@ -5,6 +5,31 @@
  */
 
 function solution(rsp) {
-  var answer = "";
+  let answer = "";
+
+  rsp = rsp.split("");
+
+  for (i = 0; i < rsp.length; i++) {
+    if (rsp[i] === 0) {
+      answer += 5;
+    } else if (rsp[i] === 2) {
+      answer += 0;
+    } else {
+      answer = +2;
+    }
+  }
+
   return answer;
 }
+
+/**
+ * function solution(rsp) {
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
+}
+ */
