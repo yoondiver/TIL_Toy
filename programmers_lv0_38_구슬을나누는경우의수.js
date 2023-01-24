c297652a7a1e62b5a03c3ae577e0d32ec2ad43f3
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-24 22:31:13 +0900

    p0-38

--- programmers_lv0_38_구슬을나누는경우의수.js
+++ programmers_lv0_38_구슬을나누는경우의수.js
@@ -16,3 +16,23 @@ function solution(balls, share) {
   }
   return factorial(balls) / (factorial(balls - share) * factorial(share));
 }
+
+/**
+ * function solution(balls, share) {
+    var result = 1;
+    while(share > 0){
+        result = result * balls / share;
+        balls = balls - 1;
+        share = share - 1;
+    }
+    return Math.round(result);
+  }
+ */
+
+/**
+ *
+ * const 팩토리얼 = (num) => num === 0 ? 1 : num * 팩토리얼(num - 1)
+ * function solution(balls, share) {
+ *  return Math.round(팩토리얼(balls) / 팩토리얼(balls - share) / 팩토리얼(share))
+ * }
+ */
