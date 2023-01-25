5fbcf8ccb59156066f34c56680ea117420e8483f
YOON <sungan1983@gmail.com>
2023-01-25 22:41:19 +0900

    p0-39

--- programmers_lv0_39_.js
+++ programmers_lv0_39_.js
@@ -5,6 +5,16 @@
  */
 
 function solution(dot) {
-  var answer = 0;
+  let answer = 0;
+
+  if (dot[0] > 0 && dot[1] > 0) {
+    answer = 1;
+  } else if (dot[0] < 0 && dot[1] > 0) {
+    answer = 2;
+  } else if (dot[0] < 0 && dot[1] < 0) {
+    answer = 3;
+  } else if (dot[0] > 0 && dot[1] < 0) {
+    answer = 4;
+  }
   return answer;
 }
