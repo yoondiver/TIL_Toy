390d030cbe5cd4f4eec0b207c903332569424f54
YOON <sungan1983@gmail.com>
2023-01-25 22:43:11 +0900

    p0-39

--- programmers_lv0_39_.js
+++ programmers_lv0_39_.js
@@ -18,3 +18,12 @@ function solution(dot) {
   }
   return answer;
 }
+
+/**
+ * 
+ * function solution(dot) {
+    const [num,num2] = dot;
+    const check = num * num2 > 0;
+    return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+  }
+ */
