b6ae6e1627db85648202f967cc8e504607c1f09b
YOON <sungan1983@gmail.com>
2023-01-27 00:48:22 +0900

    p0-43

--- programmers_lv0_43_.js
+++ programmers_lv0_43_.js
@@ -6,7 +6,6 @@
  */
 
 function solution(box, n) {
-  let answer =
-    parseInt(box[0] / n) * parseInt(box[1] / n) * parseInt(box[2] / n);
+  let answer = parseInt(box[0] / n) * parseInt(box[1] / n) * parseInt(box[2] / n);
   return answer;
 }
