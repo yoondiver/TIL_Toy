d29f243ad2743bb285638dc40f1e43d9e7c56f25
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-28 22:14:10 +0900

    p0-44

---
+++ programmers_lv0_44_.js
@@ -0,0 +1,10 @@
+// 합성수 찾기
+/**
+ * 약수의 개수가 세 개 이상인 수를 합성수라고 합니다.
+ * 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(n) {
+  var answer = 0;
+  return answer;
+}
