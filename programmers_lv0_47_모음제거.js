4f6c49dc4fa6debf85df7a55df5b5cfadd4dc6ba
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-29 22:43:36 +0900

    p0-47

--- programmers_lv0_47_모음제거.js
+++ programmers_lv0_47_모음제거.js
@@ -5,6 +5,23 @@
  */
 
 function solution(my_string) {
-  var answer = "";
-  return answer;
+  const str = "aeiou";
+
+  let newString = my_string.split("");
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < my_string.length; j++) {
+      if (newString.includes(str[i])) {
+        newString.splice(newString.indexOf(str[i]), 1);
+      }
+    }
+  }
+
+  return newString.join("");
 }
+
+/**
+ * function solution(my_string) {
+    return my_string.replace(/[aeiou]/g, '');
+  }
+ */
