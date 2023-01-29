4789360eb2b69b28e7086c729c915d6a15bbe774
YOON <50129701+yoondiver@users.noreply.github.com>
2023-01-29 22:30:18 +0900

    p0-46

---
+++ programmers_lv0_46_팩토리얼.js
@@ -0,0 +1,11 @@
+// 팩토리얼
+
+/**
+ * i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다.
+ * 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(n) {
+  var answer = 0;
+  return answer;
+}
