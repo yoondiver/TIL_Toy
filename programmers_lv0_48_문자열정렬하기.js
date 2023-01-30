19fd165fdb6516899a164235f2da83b0010b5ffe
YOON <sungan1983@gmail.com>
2023-01-30 22:35:49 +0900

    P0-48

---
+++ programmers_lv0_48_문자열정렬하기.js
@@ -0,0 +1,10 @@
+// 문자열 정렬하기 (1)
+/**
+ * 문자열 my_string이 매개변수로 주어질 때, 
+ * my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+ */
+
+function solution(my_string) {
+  var answer = [];
+  return answer;
+}
