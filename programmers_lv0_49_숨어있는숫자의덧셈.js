77dff1523b595f83c3d20ae3d0055ded2fbe7d07
YOON <sungan1983@gmail.com>
2023-01-30 22:43:54 +0900

    p0-48

---
+++ programmers_lv0_49_숨어있는숫자의덧셈.js
@@ -0,0 +1,11 @@
+// 숨어있는숫자의덧셈
+
+/**
+ * 문자열 my_string이 매개변수로 주어집니다. 
+ * my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(my_string) {
+  var answer = 0;
+  return answer;
+}
