7bd5b0648acf9c4aa505aaaca372c02f09e31a58
YOON <sungan1983@gmail.com>
2023-01-30 22:39:41 +0900

    p0-48

--- programmers_lv0_48_문자열정렬하기.js
+++ programmers_lv0_48_문자열정렬하기.js
@@ -5,6 +5,8 @@
  */
 
 function solution(my_string) {
-  var answer = [];
-  return answer;
+  let numbers = my_string.match(/[0-9]/g)//정규식을 이용해 숫자만 찾는다.
+  .map(str => parseInt(str))//string type을 number로 변환한다.
+  .sort((a,b) => a-b);//오름차순 정렬
+  return numbers;
 }
