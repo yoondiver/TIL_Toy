ac136936df4721f2a94b828adfae3b054ae02ee4
YOON <sungan1983@gmail.com>
2023-01-30 22:45:47 +0900

    p0-49

--- programmers_lv0_49_숨어있는숫자의덧셈.js
+++ programmers_lv0_49_숨어있는숫자의덧셈.js
@@ -11,3 +11,12 @@ function solution(my_string) {
   
   return answer;
 }
+
+/**
+ * function solution(my_string) {
+    const answer = my_string.replace(/[^0-9]/g, '')
+                            .split('')
+                            .reduce((acc, curr) => acc + Number(curr), 0);
+    return answer;
+  }
+ */
