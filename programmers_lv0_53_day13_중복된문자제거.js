4b9fdda0ca73d1c198f29a8b11787d9b4aa1311c
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-01 14:24:44 +0900

    p0-53

---
+++ programmers_lv0_53_day13_중복된문자제거.js
@@ -0,0 +1,11 @@
+// 중복된 문자 제거
+
+/**
+ * 문자열 my_string이 매개변수로 주어집니다.
+ * my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(my_string) {
+  var answer = "";
+  return answer;
+}
