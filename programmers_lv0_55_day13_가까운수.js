d2bddc66ab22057ea4e405514a06a2452f2f7a5c
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-02 11:31:33 +0900

    p0-55

---
+++ programmers_lv0_55_day13_가까운수.js
@@ -0,0 +1,11 @@
+// 가까운 수
+
+/**
+ * 정수 배열 array와 정수 n이 매개변수로 주어질 때,
+ * array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(array, n) {
+  var answer = 0;
+  return answer;
+}
