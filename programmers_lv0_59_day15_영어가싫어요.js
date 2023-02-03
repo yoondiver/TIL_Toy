1272abb42026735dc9dcb47bb44328f3d5473d8d
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-03 13:04:51 +0900

    p0-59

--- programmers_lv0_59_day15_영어가싫어요.js
+++ programmers_lv0_59_day15_영어가싫어요.js
@@ -23,3 +23,18 @@ function solution(numbers) {
   });
   return Number(numbers); //join된 numbers는 string이므로 number로 변환해준다.
 }
+
+/**
+ * function solution(numbers) {
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v];
+    });
+
+    return Number(num);
+  }
+ */
