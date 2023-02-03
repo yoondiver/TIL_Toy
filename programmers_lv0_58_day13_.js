88011e6edf0c79443fcd39ae148deab5ffdd210d
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-03 12:50:27 +0900

    p0-58

--- programmers_lv0_58_day13_.js
+++ programmers_lv0_58_day13_.js
@@ -1,3 +1,10 @@
+// 대문자와 소문자
+
+/**
+ * 문자열 my_string이 매개변수로 주어질 때,
+ * 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+ */
+
 function solution(order) {
   var answer = 0;
   return answer;
