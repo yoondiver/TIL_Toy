6d42d13931139fd2b91cc5480f3430f27e5ef1c0
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-04 20:48:59 +0900

    p0-60

--- programmers_lv0_60_day15_인덱스바꾸기.js
+++ programmers_lv0_60_day15_인덱스바꾸기.js
@@ -6,6 +6,11 @@
  */
 
 function solution(my_string, num1, num2) {
-  var answer = "";
+  var answer =
+    my_string.slice(0, num1) +
+    my_string[num2] +
+    my_string.slice(num1 + 1, num2) +
+    my_string[num1] +
+    my_string.slice(num2 + 1);
   return answer;
 }
