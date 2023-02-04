f23c0a0351c88878354d352e69855658a8217633
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-04 20:48:28 +0900

    p0-60

---
+++ programmers_lv0_60_day15_인덱스바꾸기.js
@@ -0,0 +1,11 @@
+// 인덱스 바꾸기
+
+/**
+ * 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때,
+ * my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+ */
+
+function solution(my_string, num1, num2) {
+  var answer = "";
+  return answer;
+}
