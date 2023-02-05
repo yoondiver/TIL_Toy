c654e62dde2d1664831a62cb69e3e45a48eaae7b
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-05 18:50:42 +0900

    p0-65

--- programmers_lv0_65_day16_문자열계산하기.js
+++ programmers_lv0_65_day16_문자열계산하기.js
@@ -7,3 +7,11 @@
 function solution(my_string) {
   return eval(my_string);
 }
+
+/**
+ * function solution(my_string) {
+    const arr = my_string.split(' ').filter(e=>e);
+    while(arr.length > 1) arr.unshift(+arr.shift() + (arr.shift() === "+" ? 1 : -1) * arr.shift())
+    return arr[0]
+  }
+ */
