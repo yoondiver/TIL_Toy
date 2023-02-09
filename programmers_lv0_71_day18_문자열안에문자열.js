c38770620a83d9f43a3238d2d1dfe8ea56f86c3b
YOON <sungan1983@gmail.com>
2023-02-10 00:48:37 +0900

    p0-71

--- programmers_lv0_71_day18_문자열안에문자열.js
+++ programmers_lv0_71_day18_문자열안에문자열.js
@@ -9,3 +9,9 @@ function solution(str1, str2) {
   var answer = str1.includes(str2) ? 1 : 2;
   return answer;
 }
+
+/**
+ * function solution(str1, str2) {
+    return str1.split(str2).length > 1 ? 1 : 2
+  }
+ */
