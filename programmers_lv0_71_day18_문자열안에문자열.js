dfee703d000a4ad2160179cf05f9f92213e2be55
YOON <sungan1983@gmail.com>
2023-02-10 00:46:06 +0900

    p0-71

---
+++ programmers_lv0_71_day18_문자열안에문자열.js
@@ -0,0 +1,11 @@
+// 문자열 안에 문자열
+
+/**
+ * 문자열 str1, str2가 매개변수로 주어집니다.
+ * str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(str1, str2) {
+  var answer = 0;
+  return answer;
+}
