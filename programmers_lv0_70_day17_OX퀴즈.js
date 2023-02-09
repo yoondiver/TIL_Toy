ebd7a4762a0b576fcdf537eabecf04199618e3ff
YOON <sungan1983@gmail.com>
2023-02-10 00:45:02 +0900

    p0-70

--- programmers_lv0_70_day17_OX퀴즈.js
+++ programmers_lv0_70_day17_OX퀴즈.js
@@ -15,3 +15,16 @@ function solution(quiz) {
   });
   return answer;
 }
+
+/**
+ * function solution(quiz) {
+    let answer = [];
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
+  }
+ */
