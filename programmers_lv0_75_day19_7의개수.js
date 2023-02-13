ad04aef1db11ca9be993eb39a8710a3bedcfcd46
YOON <sungan1983@gmail.com>
2023-02-13 21:59:37 +0900

    p0-75

---
+++ programmers_lv0_75_day19_7의개수.js
@@ -0,0 +1,10 @@
+/**
+ * 머쓱이는 행운의 숫자 7을 가장 좋아합니다.
+ * 정수 배열 array가 매개변수로 주어질 때,
+ * 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+ */
+
+function solution(array) {
+  var answer = 0;
+  return answer;
+}
