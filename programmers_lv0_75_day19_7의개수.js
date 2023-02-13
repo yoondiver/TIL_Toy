ceb60a9754056fc7fb353a048a11cc90459c201c
YOON <sungan1983@gmail.com>
2023-02-13 22:00:32 +0900

    p0-75

--- programmers_lv0_75_day19_7의개수.js
+++ programmers_lv0_75_day19_7의개수.js
@@ -5,6 +5,11 @@
  */
 
 function solution(array) {
-  var answer = 0;
+  let answer = 0;
+  let str = array.toString();
+  for (let i = 0; i < str.length; i++)
+    if (str[i] == "7") {
+      answer++;
+    }
   return answer;
 }
