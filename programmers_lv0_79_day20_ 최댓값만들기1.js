3e6aeef7949936794b6d7e3f503b1f2090ba9ff1
YOON <sungan1983@gmail.com>
2023-02-16 21:01:34 +0900

    p0-79

---
+++ programmers_lv0_79_day20_ 최댓값만들기1.js
@@ -0,0 +1,12 @@
+// 최대값 만들기1
+
+/**
+ * 정수 배열 numbers가 매개변수로 주어집니다.
+ * numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을
+ * return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(numbers) {
+  var answer = 0;
+  return answer;
+}
