8111c8d0c0c32b4e34b3ee88be24e763035168a9
YOON <sungan1983@gmail.com>
2023-02-16 21:13:40 +0900

    p0-80

---
+++ programmers_lv0_80_day20_ 다항식더하기.js
@@ -0,0 +1,10 @@
+// 다항식 더하기
+
+/**
+ * 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다. 다항식을 계산할 때는 동류항끼리 계산해 정리합니다. 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 같은 식이라면 가장 짧은 수식을 return 합니다.
+ */
+
+function solution(polynomial) {
+  var answer = "";
+  return answer;
+}
