d0640d9f7b62209a87fd570dccd4b917ac98f26a
YOON <sungan1983@gmail.com>
2023-02-17 23:40:46 +0900

    p0-81

--- programmers_lv0_81_day21_ 숨어있는숫자의덧셈2.js
+++ programmers_lv0_81_day21_ 숨어있는숫자의덧셈2.js
@@ -1,7 +1,9 @@
 // 숨어있는 숫자의 덧셈(2)
 
 /**
- * 문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+ * 문자열 my_string이 매개변수로 주어집니다.
+ * my_string은 소문자, 대문자, 자연수로만 구성되어있습니다.
+ * my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
  */
 
 function solution(my_string) {
