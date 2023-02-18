41eff2d74dc8d90956912761c5751c797caae4e0
YOON <sungan1983@gmail.com>
2023-02-18 23:26:24 +0900

    p0-82

--- programmers_lv0_82_day21_안전지대.js
+++ programmers_lv0_82_day21_안전지대.js
@@ -36,3 +36,19 @@ function solution(board) {
   }
   return answer;
 }
+
+/*
+function solution(board) {
+
+    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
+    let safezone = 0;
+
+    board.forEach((row, y, self) => row.forEach((it, x) => {
+        if (it === 1) return false;
+        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
+               ? false : safezone++;
+    }));
+
+    return safezone;
+}
+*/
