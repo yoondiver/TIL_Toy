3f4554aaecce6f9311fe3fd2a9b8307bad1771ff
YOON <sungan1983@gmail.com>
2023-02-22 22:46:22 +0900

    p0-86

--- programmers_lv0_86_day22_평행.js
+++ programmers_lv0_86_day22_평행.js
@@ -7,5 +7,22 @@
 
 function solution(dots) {
   var answer = 0;
-  return answer;
+
+  function calculation(a, b, c, d) {
+    let abDiff, cdDiff;
+
+    abDiff = (b[1] - a[1]) / (b[0] - a[0]);
+    cdDiff = (d[1] - c[1]) / (d[0] - c[0]);
+
+    if (abDiff === cdDiff) {
+      // 기울기가 같으면? +1
+      answer += 1;
+    }
+  }
+
+  calculation(dots[0], dots[1], dots[2], dots[3]);
+  calculation(dots[0], dots[2], dots[1], dots[3]);
+  calculation(dots[0], dots[3], dots[1], dots[2]);
+
+  return answer > 0 ? 1 : 0;
 }
