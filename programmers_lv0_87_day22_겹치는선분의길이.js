7fbe7c8a885ca89201373934f5c6cb0ff4999b09
YOON <sungan1983@gmail.com>
2023-02-23 23:04:17 +0900

    p0-87

--- programmers_lv0_87_day22_겹치는선분의길이.js
+++ programmers_lv0_87_day22_겹치는선분의길이.js
@@ -25,3 +25,15 @@ function solution(lines) {
 
   return intersection;
 }
+
+/**
+ * function solution(lines) {
+    let line = new Array(200).fill(0);
+
+    lines.forEach(([a, b]) => {
+        for(; a < b; a++) line[a+100]++;
+    });
+
+    return line.reduce((a, c) =>  c > 1 ? a + 1 : a, 0)
+}
+ */
