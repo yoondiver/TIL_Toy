694312634c3cc5a1b395ff840b08d1e45abf8e91
YOON <sungan1983@gmail.com>
2023-02-24 01:21:03 +0900

    p0-88

--- programmers_lv0_88_day22_유한소수판별하기.js
+++ programmers_lv0_88_day22_유한소수판별하기.js
@@ -8,8 +8,33 @@
  */
 
 function solution(a, b) {
-  var answer = 0;
-  return answer;
+  let curA = a;
+  let curB = b;
+
+  while (true) {
+    for (let i = 2; i <= Math.min(curA, curB); i++) {
+      if (!(curA % i) && !(curB % i)) {
+        curA /= i;
+        curB /= i;
+        continue;
+      }
+    }
+    break;
+  }
+
+  while (true) {
+    if (!(curB % 2)) {
+      curB /= 2;
+      continue;
+    } else if (!(curB % 5)) {
+      curB /= 5;
+      continue;
+    } else if (curB === 1) {
+      return 1;
+    } else {
+      return 2;
+    }
+  }
 }
 
 /**
