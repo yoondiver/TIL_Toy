aef71990458ec4481faf76a5ce06742ffa49f862
YOON <sungan1983@gmail.com>
2023-02-23 23:01:55 +0900

    p0-87

---
+++ programmers_lv0_87_day22_겹치는선분의길이.js
@@ -0,0 +1,10 @@
+/**
+ * 선분 3개가 평행하게 놓여 있습니다. 세 선분의 시작과 끝 좌표가 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때, 두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해보세요.
+
+lines가 [[0, 2], [-3, -1], [-2, 1]]일 때 그림으로 나타내면 다음과 같습니다.
+ */
+
+function solution(lines) {
+  var answer = 0;
+  return answer;
+}
