e2034013402a75773aa25d98f667b55877ebfa8b
YOON <50129701+yoondiver@users.noreply.github.com>
2023-02-26 03:47:39 +0900

    p0-90

---
+++ programmers_lv0_90_day23_.js
@@ -0,0 +1,8 @@
+/**
+ * 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(score) {
+  var answer = [];
+  return answer;
+}
