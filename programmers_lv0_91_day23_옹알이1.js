843bbfe0d8d10ad288b6679423f713ae7f61fdf1
YOON <sungan1983@gmail.com>
2023-02-28 18:54:47 +0900

    p0-91

--- programmers_lv0_91_day23_옹알이1.js
+++ programmers_lv0_91_day23_옹알이1.js
@@ -3,6 +3,20 @@
  */
 
 function solution(babbling) {
-  var answer = 0;
+  return babbling
+    .map((_) => _.replace(/aya|ye|woo|ma/g, ""))
+    .filter((str) => !str).length;
+}
+
+/*
+function solution(babbling) {
+  let answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach((word) => {
+    if (regex.test(word)) answer++;
+  });
+
   return answer;
 }
+*/
