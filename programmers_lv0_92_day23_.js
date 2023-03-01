6b6a0a37ce2d7f00a1a8dddd5f99d76c8a9d4876
YOON <sungan1983@gmail.com>
2023-03-01 23:28:56 +0900

    p0-92

--- programmers_lv0_92_day23_.js
+++ programmers_lv0_92_day23_.js
@@ -6,6 +6,8 @@
  */
 
 function solution(id_pw, db) {
-  var answer = "";
-  return answer;
+  const userData = db.find(([el]) => el == id_pw[0]);
+  if (userData) {
+    return userData[1] == id_pw[1] ? "login" : "wrong pw";
+  } else return "fail";
 }
