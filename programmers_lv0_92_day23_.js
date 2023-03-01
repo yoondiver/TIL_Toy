58ce503cbf67450ec6b13063a40d374f4affc587
YOON <sungan1983@gmail.com>
2023-03-01 23:27:35 +0900

    p0-92

--- programmers_lv0_92_day23_.js
+++ programmers_lv0_92_day23_.js
@@ -4,3 +4,8 @@
 아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
 로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
  */
+
+function solution(id_pw, db) {
+  var answer = "";
+  return answer;
+}
