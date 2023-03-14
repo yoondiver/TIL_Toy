98a2b9ad7612ac06b538b7e2c85192840620e8ff
YOON <50129701+yoondiver@users.noreply.github.com>
2023-03-14 10:26:27 +0900

    p0-99

--- programmers_lv0_99_day25_연속된수의합.js
+++ programmers_lv0_99_day25_연속된수의합.js
@@ -3,6 +3,35 @@
  */
 
 function solution(num, total) {
-  var answer = [];
+  let answer = [];
+  let zeroSum = 0;
+  let start = 0;
+  for (let i = 0; i < num; i++) {
+    zeroSum += i;
+  }
+  for (let k = 0; k < 100; k++) {
+    if (total > zeroSum) {
+      zeroSum += num;
+      start++;
+    } else if (total < zeroSum) {
+      zeroSum -= num;
+      start--;
+    } else if (total === zeroSum) {
+      break;
+    }
+  }
+  for (let j = 0; j < num; j++) {
+    answer.push(start);
+    start++;
+  }
   return answer;
 }
+
+/**
+ * function solution(num, total) {
+    var min = Math.ceil(total/num - Math.floor(num/2));
+    var max = Math.floor(total/num + Math.floor(num/2));
+
+    return new Array(max-min+1).fill(0).map((el,i)=>{return i+min;});
+}
+ */
