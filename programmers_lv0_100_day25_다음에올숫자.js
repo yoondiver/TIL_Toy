780843f877465930c25f56fe79b7032a4254f827
YOON <sungan1983@gmail.com>
2023-03-16 21:50:27 +0900

    p0-100

--- programmers_lv0_100_day25_다음에올숫자.js
+++ programmers_lv0_100_day25_다음에올숫자.js
@@ -4,5 +4,13 @@
 
 function solution(common) {
   var answer = 0;
+  // 등차인 경우
+  if (common[1] - common[0] == common[2] - common[1]) {
+    answer = common.pop() + (common[1] - common[0]);
+  }
+  // 등비인 경우
+  if (common[1] / common[0] == common[2] / common[1]) {
+    answer = common.pop() * (common[1] / common[0]);
+  }
   return answer;
 }
