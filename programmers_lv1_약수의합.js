5091c043142c3f24cd472a2a16f88ee95e80a29f
YOON <sungan1983@gmail.com>
2023-03-21 01:39:37 +0900

    p1-2

--- programmers_lv1_약수의합.js
+++ programmers_lv1_약수의합.js
@@ -15,3 +15,13 @@ function solution(n) {
 
   return answer;
 }
+
+/**
+ * function solution(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) sum += i
+    }
+    return sum
+}
+ */
