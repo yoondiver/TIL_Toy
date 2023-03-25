ed63f14524339561913cd2fb89e31565f6e45d41
YOON <sungan1983@gmail.com>
2023-03-25 17:13:48 +0900

    p1-4

---
+++ programmers_lv1_04_자릿수더하기.js
@@ -0,0 +1,13 @@
+/**
+ * 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+ */
+
+function solution(n) {
+  var answer = 0;
+
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  console.log("Hello Javascript");
+
+  return answer;
+}
