69bd14182a3333162626340976fabb7a60c797bb
YOON <sungan1983@gmail.com>
2023-03-30 02:06:01 +0900

    p1-7

--- programmers_lv1_07_문자열내p와y의개수.js
+++ programmers_lv1_07_문자열내p와y의개수.js
@@ -28,3 +28,15 @@ function solution(s) {
   }
   return answer;
 }
+
+/*
+function solution(s) {
+  return [...s.toLowerCase()].reduce((acc, cur) => {
+    if (cur === "p") return acc + 1;
+    else if (cur === "y") return acc - 1;
+    return acc;
+  }, 0)
+    ? false
+    : true;
+}
+*/
