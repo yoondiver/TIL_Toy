41ac01a8c8e26a744c63ef832385c0987fc23f5f
YOON <sungan1983@gmail.com>
2023-03-30 02:04:00 +0900

    p1-7

--- programmers_lv1_07_문자열내p와y의개수.js
+++ programmers_lv1_07_문자열내p와y의개수.js
@@ -6,9 +6,25 @@
 
 function solution(s) {
   var answer = true;
-
-  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log("Hello Javascript");
-
+  let text = s.toLowerCase();
+  let p = "";
+  let y = "";
+  for (i = 0; i < s.length; i++) {
+    if (text[i] === "p") {
+      p += text[i];
+    } else if (text[i] === "y") {
+      y += text[i];
+    }
+  }
+  if (p.length === y.length) {
+    return (answer = true);
+  } else {
+    answer = false;
+  }
+  if (p.length === 0 && y.length === 0) {
+    return (answer = true);
+  } else {
+    answer = false;
+  }
   return answer;
 }
