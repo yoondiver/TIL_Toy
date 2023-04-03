ac7b5ad062f8deae2a31c7cbf14380ebde9cd2d9
YOON <sungan1983@gmail.com>
2023-04-03 22:07:56 +0900

    p1-9

--- programmers_lv1_09_자연수 뒤집어 배열로 만들.js
+++ programmers_lv1_09_자연수 뒤집어 배열로 만들.js
@@ -12,3 +12,20 @@ function solution(n) {
   }
   return answer;
 }
+
+/*
+function solution(n) {
+  // 문자풀이
+  // return (n+"").split("").reverse().map(v => parseInt(v));
+
+  // 숫자풀이
+  var arr = [];
+
+  do {
+    arr.push(n % 10);
+    n = Math.floor(n / 10);
+  } while (n > 0);
+
+  return arr;
+}
+*/
