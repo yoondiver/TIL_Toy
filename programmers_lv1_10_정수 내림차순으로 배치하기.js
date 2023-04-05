c5830e8baa68fe4dea0db8d78104db7abaa66193
YOON <sungan1983@gmail.com>
2023-04-05 17:51:22 +0900

    p1-10

--- programmers_lv1_10_정수 내림차순으로 배치하기.js
+++ programmers_lv1_10_정수 내림차순으로 배치하기.js
@@ -5,6 +5,44 @@
  */
 
 function solution(n) {
-  var answer = 0;
-  return answer;
+  let result = n.toString().split("").sort().reverse().join("");
+  return parseInt(result);
 }
+
+/*
+function solution(n) {
+  const newN = n + "";
+  const newArr = newN.split("").sort().reverse().join("");
+
+  return +newArr;
+}
+*/
+
+/*
+function solution(n) {
+  // 문자풀이
+  // return parseInt((n+"").split("").sort().reverse().join(""));
+
+  // 숫자풀이
+  var r = 0,
+    e = 0,
+    arr = [];
+
+  do {
+    e = n % 10;
+
+    // 정렬
+    if (arr.length == 0) arr.push(e);
+    else
+      for (var i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] <= e) {
+          arr.splice(i, 0, e);
+          break;
+        }
+        if (i == len - 1) arr.push(e);
+      }
+  } while (((n = Math.floor(n / 10)), n > 0));
+
+  return parseInt(arr.join(""));
+}
+*/
