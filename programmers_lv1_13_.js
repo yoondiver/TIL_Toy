f6da4a50bafa4acea4cf28f5b9d76792f770bf86
YOON <50129701+yoondiver@users.noreply.github.com>
2023-04-11 18:52:36 +0900

    p1-13

--- programmers_lv1_13_.js
+++ programmers_lv1_13_.js
@@ -19,3 +19,15 @@ function solution(a, b) {
 
   return answer;
 }
+
+/**
+function solution(a, b) {
+  let answer = 0;
+
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer += i;
+  }
+
+  return answer;
+}
+*/
