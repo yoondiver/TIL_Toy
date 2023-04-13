dd319fbe011f8bd3e3ef1121fc20ad8cbb428635
YOON <sungan1983@gmail.com>
2023-04-13 23:23:13 +0900

    p1-14

--- programmers_lv1_14_콜라츠추측.js
+++ programmers_lv1_14_콜라츠추측.js
@@ -42,3 +42,17 @@ function solution(num) {
 
   return answer;
 }
+
+function solution(num) {
+  var count = 0;
+
+  while (count < 500) {
+    if (num === 1) {
+      return count;
+    }
+    count++;
+    num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+  }
+
+  return -1;
+}
