9fa1854bf78c7011b35a0671b4e1bb5bbfb04ad6
YOON <sungan1983@gmail.com>
2023-04-17 23:36:44 +0900

    p1-16

--- programmers_lv1_16_나누어 떨어지는 숫자 배열.js
+++ programmers_lv1_16_나누어 떨어지는 숫자 배열.js
@@ -25,3 +25,10 @@ function solution(arr, divisor) {
 
   return answer;
 }
+
+/*
+function solution(arr, divisor) {
+    var answer = arr.filter(v => v%divisor == 0);
+    return answer.length == 0 ? [-1] : answer.sort((a,b) => a-b);
+}
+*/
