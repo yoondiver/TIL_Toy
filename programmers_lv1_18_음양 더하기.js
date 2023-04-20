1f0ada7c1fec51e2daac08ddf82d3f18bf84f763
YOON <50129701+yoondiver@users.noreply.github.com>
2023-04-20 23:16:13 +0900

    p1-18

--- programmers_lv1_18_음양 더하기.js
+++ programmers_lv1_18_음양 더하기.js
@@ -4,3 +4,8 @@
  * 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다.
  * 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
  */
+
+function solution(absolutes, signs) {
+  var answer = 123456789;
+  return answer;
+}
