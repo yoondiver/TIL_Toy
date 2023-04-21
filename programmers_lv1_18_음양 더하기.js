c390d25a5e78d2e0e087d82164e53fddefb66c6b
YOON <sungan1983@gmail.com>
2023-04-21 23:21:47 +0900

    p1-18

--- programmers_lv1_18_음양 더하기.js
+++ programmers_lv1_18_음양 더하기.js
@@ -11,3 +11,15 @@ function solution(absolutes, signs) {
     0
   );
 }
+
+function solution(absolutes, signs) {
+  let answer = 0;
+  absolutes.forEach((v, i) => {
+    if (signs[i]) {
+      answer += v;
+    } else {
+      answer -= v;
+    }
+  });
+  return answer;
+}
