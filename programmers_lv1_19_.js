6ba618c21d126c16350cb3f8083c50b80f471f42
YOON <sungan1983@gmail.com>
2023-04-23 11:27:30 +0900

    p1-19

--- programmers_lv1_19_.js
+++ programmers_lv1_19_.js
@@ -20,3 +20,11 @@ function solution(arr) {
 
   return answer;
 }
+
+/**
+ * function solution(arr) {
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  if (arr.length < 1) return [-1];
+  return arr;
+}
+*/
