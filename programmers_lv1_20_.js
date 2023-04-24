278c1c4c8a0d704b40a84d83fc7508fafd86a40c
YOON <sungan1983@gmail.com>
2023-04-25 01:59:57 +0900

    p1-20

--- programmers_lv1_20_.js
+++
@@ -1,8 +0,0 @@
-/**
- * 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
- */
-
-function solution(numbers) {
-  var answer = -1;
-  return answer;
-}
