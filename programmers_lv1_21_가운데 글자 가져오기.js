ee226ce8fee15b4683726082f85b99b9ff908c41
YOON <sungan1983@gmail.com>
2023-04-29 16:30:23 +0900

    p1-21

--- programmers_lv1_21_가운데 글자 가져오기.js
+++ programmers_lv1_21_가운데 글자 가져오기.js
@@ -6,7 +6,7 @@ function solution(s) {
   let answer = "";
   let mid = s.length / 2;
 
-  answer *= (s.length % 2 === 0)  ? s.substr(mid-1, 2)
+  answer *= s.length % 2 === 0 ? s.substr(mid - 1, 2) : s.substr(mid, 1);
 
   return answer;
 }
