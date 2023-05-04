d595d6ec452f6e4083066cd60f3fad27c82cc13b
YOON <sungan1983@gmail.com>
2023-05-04 23:03:29 +0900

    p1-23

--- programmers_lv1_23_내적.js
+++ programmers_lv1_23_내적.js
@@ -5,10 +5,10 @@
  */
 
 function solution(a, b) {
-  var answer = 1234567890;
+  var answer = 0;
 
-  for (let i = 0; i < a; i++) {
-    answer += a[i] + b[i];
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
   }
 
   return answer;
