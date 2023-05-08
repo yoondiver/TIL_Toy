14ce90924415b83c175d93a0c1b243ab9d547e73
YOON <sungan1983@gmail.com>
2023-05-09 00:21:28 +0900

    p1-25

--- programmers_lv1_25_약수의 개수와 덧셈.js
+++ programmers_lv1_25_약수의 개수와 덧셈.js
@@ -20,7 +20,7 @@ function solution(left, right) {
 
   let result = 0;
 
-  for (let j = 0; j <= right; j++) {
+  for (let j = left; j <= right; j++) {
     if (dcn(j) % 2 === 0) {
       result = result + j;
     } else {
@@ -29,3 +29,17 @@ function solution(left, right) {
   }
   return result;
 }
+
+/**
+ * function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
+ */
