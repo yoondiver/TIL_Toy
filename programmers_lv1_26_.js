4dedc1450770560a9e597dd052f00e000b695bfb
YOON <sungan1983@gmail.com>
2023-05-09 23:54:18 +0900

    p1-26

--- programmers_lv1_26_.js
+++ programmers_lv1_26_.js
@@ -3,3 +3,9 @@
 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
 단, 금액이 부족하지 않으면 0을 return 하세요.
  */
+
+function solution(price, money, count) {
+  var answer = -1;
+
+  return answer;
+}
