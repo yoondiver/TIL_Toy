708be9923f573278b06309c366f524ace448a5ad
YOON <sungan1983@gmail.com>
2023-05-11 23:25:19 +0900

    p1-26

--- programmers_lv1_26_부족한 금액 계산하기.js
+++ programmers_lv1_26_부족한 금액 계산하기.js
@@ -20,3 +20,10 @@ function solution(price, money, count) {
 
   return answer;
 }
+
+/**
+ * function solution(price, money, count) {
+    const tmp = price * count * (count + 1) / 2 - money;
+    return tmp > 0 ? tmp : 0;
+}
+ */
