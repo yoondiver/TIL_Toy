7a4966b37d275a07d4f5e0fdfc1082511cd39943
YOON <sungan1983@gmail.com>
2023-05-18 20:48:22 +0900

    p1-27

--- programmers_lv1_27_문자열 다루기 기본.js
+++ programmers_lv1_27_문자열 다루기 기본.js
@@ -6,3 +6,13 @@
 function solution(s) {
   return (s.length === 4 || s.length === 6) && s == parseInt(s);
 }
+
+/*
+function solution(s) {
+  if(s.length != 4 && s.length != 6) return false;
+  for(var i = 0; i < s.length; i++) {
+      if(isNaN(Number(s[i]))) return false;
+  }
+  return true;
+}
+*/
