f3544b4a23af9508319ad01a3ce83cbb3fff8f0a
YOON <sungan1983@gmail.com>
2023-05-23 22:09:48 +0900

    p1-28

--- programmers_lv1_28_행렬의 덧셈.js
+++ programmers_lv1_28_행렬의 덧셈.js
@@ -4,7 +4,8 @@
 
 function solution(arr1, arr2) {
   return arr1.map((row, indexOfRow) => {
-    return row.map((col, indexOfCol) => {});
-    return arr2;
+    return row.map((col, indexOfCol) => {
+      return col + arr2[indexOfRow][indexOfCol];
+    });
   });
 }
