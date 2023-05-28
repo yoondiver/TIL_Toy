c16fa9cd14dd37724ce80745004afc66237bb84a
YOON <50129701+yoondiver@users.noreply.github.com>
2023-05-28 15:06:55 +0900

    p1-29

--- programmers_lv1_29_직사각형 별찍기.js
+++ programmers_lv1_29_직사각형 별찍기.js
@@ -6,7 +6,7 @@
 process.stdin.setEncoding("utf8");
 process.stdin.on("data", (data) => {
   const n = data.split(" ");
-  const a = Number(n[0]),
+  const col = Number(n[0]),
     b = Number(n[1]);
   console.log(a);
   console.log(b);
