243433511a82b13efb5e38d16ace3e23bc235d46
YOON <50129701+yoondiver@users.noreply.github.com>
2023-05-28 15:10:37 +0900

    p1-29

--- programmers_lv1_29_직사각형 별찍기.js
+++ programmers_lv1_29_직사각형 별찍기.js
@@ -11,3 +11,16 @@ process.stdin.on("data", (data) => {
   const str = `${"*".repeat(col)}\n`.repeat(row);
   console.log(str);
 });
+
+/*
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
+    }
+
+});
+ */
