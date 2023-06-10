5000567b2bbdfb00e57396d6576aaac3f645848a
YOON <50129701+yoondiver@users.noreply.github.com>
2023-06-10 21:35:27 +0900

    p1-30

--- programmers_lv1_30_최대공약수와 최소공배수.js
+++ programmers_lv1_30_최대공약수와 최소공배수.js
@@ -52,3 +52,17 @@ function solution(n, m) {
 
   return answer;
 }
+
+/*
+function solution(n, m) {
+    const min = Math.min(n, m);
+    let common_max = 1;
+    for(let i = min; i > 0; i--){
+        if(n % i === 0 && m % i === 0){
+            common_max = i;
+            break;
+        }
+    }
+    return [common_max, common_max * (n / common_max)* (m / common_max)];
+}
+*/
