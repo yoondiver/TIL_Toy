f3779bfac2962ddf08b3036c3238cacf79015fd2
YOON <50129701+yoondiver@users.noreply.github.com>
2023-06-13 11:41:07 +0900

    p1-31

--- programmers_lv1_31_.js
+++ programmers_lv1_31_.js
@@ -5,3 +5,12 @@ arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
 arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
  */
+
+function solution(arr) {
+  var answer = [];
+
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  console.log("Hello Javascript");
+
+  return answer;
+}
