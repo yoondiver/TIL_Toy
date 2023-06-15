0f59c631e44e40884f32ed5947cffb3d1e295583
YOON <sungan1983@gmail.com>
2023-06-16 00:55:27 +0900

    p1-31

--- programmers_lv1_31_같은 숫자는 싫어.js
+++ programmers_lv1_31_같은 숫자는 싫어.js
@@ -10,7 +10,9 @@ function solution(arr) {
   var answer = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] !== arr[i+1])
+    if (arr[i] !== arr[i + 1]) {
+      answer.push(arr[i]);
+    }
   }
   return arr.reduce(reducer, []);
 }
