0a5ad85547e31d6ed657766a3d06fc9df000817d
YOON <50129701+yoondiver@users.noreply.github.com>
2023-06-24 23:37:37 +0900

    p1-32

--- programmers_lv1_32_3진법 뒤집기.js
+++ programmers_lv1_32_3진법 뒤집기.js
@@ -7,3 +7,12 @@ function solution(n) {
 
   return parseInt(n, 3);
 }
+
+/**
+ * function solution(n) {
+  return n
+    .toString(3)
+    .split("")
+    .reduce((acc, cur, i) => acc + cur * 3 ** i, 0);
+}
+ */
