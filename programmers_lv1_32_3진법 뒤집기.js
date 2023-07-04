b9b437c6845de2d210f147467946e9c7bca25b89
YOON <sungan1983@gmail.com>
2023-07-04 15:01:19 +0900

    p1-32

--- programmers_lv1_32_3진법 뒤집기.js
+++ programmers_lv1_32_3진법 뒤집기.js
@@ -17,12 +17,14 @@ function solution(n) {
 }
  */
 
+/*
 function solution(n) {
   return n
     .toString(3)
     .split("")
     .reduce((acc, cur, i) => acc + cur * 3 * i, 0);
 }
+*/
 
 function solution(n) {
   const result = [];
