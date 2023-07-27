f94298ef9677e8bd5e311ea3a6e43b2e9c4a2b55
YOON <50129701+yoondiver@users.noreply.github.com>
2023-07-27 23:38:16 +0900

    p1-34

--- programmers_lv1_34_예산.js
+++ programmers_lv1_34_예산.js
@@ -7,3 +7,8 @@
  * 부서별로 신청한 금액이 들어있는 배열 d와 예산 budget이 매개변수로 주어질 때,
  * 최대 몇 개의 부서에 물품을 지원할 수 있는지 return 하도록 solution 함수를 완성해주세요.
  */
+
+function solution(d, budget) {
+  var answer = 0;
+  return answer;
+}
