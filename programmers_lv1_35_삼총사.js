61265aef171254ae2e263f145d59de6a90588cca
YOON <50129701+yoondiver@users.noreply.github.com>
2023-08-19 23:21:25 +0900

    p1-35

--- programmers_lv1_35_삼총사.js
+++ programmers_lv1_35_삼총사.js
@@ -9,12 +9,11 @@
 function solution(number) {
   let result = 0;
   for (let i = 0; i < number.length - 2; i++) {
-    for (let j = i + 1; j < number.length - 1; j++) {
-      for (let k = j + 1; k < number.length; k++) {
-        const sum = number[i] + number[j] + number[k];
-        if (number === 0) return (result = 1);
+    for (let j = i + 1; i < number.length - 1; j++) {
+      for (let k = j + 1; i < number.length; k++) {
+        if (number[i] + number[j] + number[k] == 0) return (result += 1);
       }
     }
   }
-  return answer;
+  return result;
 }
