a9ef76b368191d4b66fd4ac2c262c6d3b21426d5
YOON <50129701+yoondiver@users.noreply.github.com>
2023-08-22 14:38:57 +0900

    p1-35

--- programmers_lv1_35_삼총사.js
+++ programmers_lv1_35_삼총사.js
@@ -46,7 +46,7 @@ function solution(number) {
     }
 
     for (let i = start; i < number.length; i++) {
-      combination(...current, number);
+      combination(...current, number, i);
     }
   };
 
