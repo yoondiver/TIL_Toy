172c762110b943d2ed7e71582d2d614d0af90964
YOON <50129701+yoondiver@users.noreply.github.com>
2023-08-26 01:33:19 +0900

    ㅔ1-35

--- programmers_lv1_35_삼총사.js
+++ programmers_lv1_35_삼총사.js
@@ -38,20 +38,19 @@ function solution(number) {
 
 /*
 function solution(number) {
-  let result = 0;
+    let result = 0;
 
-  const combination = (current, start) => {
-    if (current.length === 3) {
-      result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
-      return;
+    const combination = (current, start) => {
+        if (current.length === 3) {
+            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+            return;
+        }
+
+        for (let i = start; i < number.length; i++) {
+            combination([...current, number[i]], i + 1);
+        }
     }
-
-    for (let i = start; i < number.length; i++) {
-      combination([...current, number[i]], i + 1);
-    }
-  };
-
-  combination([], 0);
-  return result;
+    combination([], 0);
+    return result;
 }
 */
