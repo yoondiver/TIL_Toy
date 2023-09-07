2eed7c78a00f92e352d50a1a0c2a3e3e9ef94ebb
YOON <50129701+yoondiver@users.noreply.github.com>
2023-09-07 19:49:28 +0900

    p1-36

--- programmers_lv1_36_최소직사각형.js
+++ programmers_lv1_36_최소직사각형.js
@@ -20,13 +20,11 @@
 function solution(sizes) {
   let max = [0, 0];
   let arr = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]));
-
   for (let x of arr) {
     if (x[0] > max[0]) max[0] = x[0];
     if (x[1] > max[1]) max[1] = x[1];
   }
-  var answer = 0;
-  return max[0] + max[1];
+  return max[0] * max[1];
 }
 
 /*
