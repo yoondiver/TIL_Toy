d99238b36009bf125dd7026b38a33c5bd91cf74e
YOON <sungan1983@gmail.com>
2023-09-19 22:19:39 +0900

    p1-37

--- programmers_lv1_37_크기가 작은 부분문자열.js
+++ programmers_lv1_37_크기가 작은 부분문자열.js
@@ -28,3 +28,16 @@ function solution(t, p) {
 
   return result; // 결과 값 반환
 }
+
+/*
+
+function solution(t, p) {
+    let count = 0;
+    for(let i=0; i<=t.length-p.length; i++) {
+        let value = t.slice(i, i+p.length);
+        if(+p >= +value) count++;
+    }
+    return count;
+}
+
+*/
