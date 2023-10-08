8d4204d1407d89d4d655abb8200cacdc84b33a34
YOON <sungan1983@gmail.com>
2023-10-08 12:38:10 +0900

    p1-39

--- programmers_lv1_39_숫자 문자열과 영단어.js
+++ programmers_lv1_39_숫자 문자열과 영단어.js
@@ -61,3 +61,17 @@ function solution(s) {
     return Number(s)
 }
 */
+
+/*
+function solution(s) {
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  var answer = s;
+
+  for(let i=0; i< numbers.length; i++) {
+      let arr = answer.split(numbers[i]);
+      answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
+*/
