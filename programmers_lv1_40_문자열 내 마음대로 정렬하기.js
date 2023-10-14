4248e78e7ad8109eedeb1d01cd0b0b52dc8d861f
YOON <sungan1983@gmail.com>
2023-10-14 13:35:33 +0900

    p1-40

--- programmers_lv1_40_문자열 내 마음대로 정렬하기.js
+++ programmers_lv1_40_문자열 내 마음대로 정렬하기.js
@@ -20,14 +20,13 @@ strings	n	return
 "abce"와 "abcd", "cdx"의 2번째 인덱스 값은 "c", "c", "x"입니다. 따라서 정렬 후에는 "cdx"가 가장 뒤에 위치합니다. "abce"와 "abcd"는 사전순으로 정렬하면 "abcd"가 우선하므로, 답은 ["abcd", "abce", "cdx"] 입니다.*/
 
 function solution(strings, n) {
-  var answer = [];
-
   let nStr = "";
   strings.forEach((str, i) => {
     nStr = str.substr(n, 1);
     strings[i] = nStr + str;
   });
   strings.sort();
+  strings.forEach((str, i) => {});
 
   return strings;
 }
