8b2e74fceff83e3a2cc190b78ecd540b66950387
YOON <50129701+yoondiver@users.noreply.github.com>
2023-10-17 21:44:49 +0900

    p1-40

--- programmers_lv1_40_문자열 내 마음대로 정렬하기.js
+++ programmers_lv1_40_문자열 내 마음대로 정렬하기.js
@@ -36,3 +36,20 @@ function solution(strings, n) {
 
   return strings;
 }
+
+/*
+function solution(strings, n) {
+    var answer = [];
+    for (var i = 0; i < strings.length; i++) {
+        var chu = strings[i][n];
+        strings[i] = chu + strings[i];
+    }
+    strings.sort();
+    for (var j = 0; j < strings.length; j++) {
+        strings[j] = strings[j].replace(strings[j][0],"");
+        answer.push(strings[j])
+    }
+
+    return answer;
+}
+*/
