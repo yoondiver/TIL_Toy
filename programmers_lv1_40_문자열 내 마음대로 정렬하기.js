3cc1e0359a6de9498f7f49bd664a3a70a680dd2a
YOON <sungan1983@gmail.com>
2023-10-18 23:46:54 +0900

    p1-40

--- programmers_lv1_40_문자열 내 마음대로 정렬하기.js
+++ programmers_lv1_40_문자열 내 마음대로 정렬하기.js
@@ -45,6 +45,7 @@ function solution(strings, n) {
         strings[i] = chu + strings[i];
     }
     strings.sort();
+    
     for (var j = 0; j < strings.length; j++) {
         strings[j] = strings[j].replace(strings[j][0],"");
         answer.push(strings[j])
