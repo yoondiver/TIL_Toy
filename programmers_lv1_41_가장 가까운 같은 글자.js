7bb4344d4f8e2abe520082a9798f6ca88e20b6f2
YOON <50129701+yoondiver@users.noreply.github.com>
2023-11-07 22:59:46 +0900

    p1-41

--- programmers_lv1_41_가장 가까운 같은 글자.js
+++ programmers_lv1_41_가장 가까운 같은 글자.js
@@ -52,3 +52,13 @@ function solution(s) {
   return result;
 }
 */
+
+function solution(s) {
+  const hash = {};
+
+  return [...s].map((v, i) => {
+    let result = hash[v] !== undefined ? i - hash[v] : -1;
+    hash[v] = i;
+    return result;
+  });
+}
