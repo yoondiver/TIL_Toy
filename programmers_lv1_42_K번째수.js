e03df3013e61325f89ca7a3dcb518ff129c8a9dd
YOON <50129701+yoondiver@users.noreply.github.com>
2023-11-24 22:54:15 +0900

    p1-42

--- programmers_lv1_42_K번째수.js
+++ programmers_lv1_42_K번째수.js
@@ -53,7 +53,7 @@ console.log(testA);
 function solution(array, commands) {
   return commands.map((command) => {
     const [sPosition, ePosition, position] = command;
-    const newArray = array;
+    const newArray = array.filter(value);
   });
   return newArray;
 }
