5b005ff94fe62bb59df34b5b484893093c0562b6
yoondiver <sungan1983@gmail.com>
2023-11-30 23:36:06 +0900

    p1-42

--- programmers_lv1_42_K번째수.js
+++ programmers_lv1_42_K번째수.js
@@ -54,7 +54,9 @@ function solution(array, commands) {
   return commands.map((command) => {
     const [sPosition, ePosition, position] = command;
     const newArray = array
-      .filter((value) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+      .filter(
+        (value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1
+      )
       .sort((a, b) => a - b);
 
     return newArray[position - 1];
