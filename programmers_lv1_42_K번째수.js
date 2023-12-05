68308857a48cb75f0a6d95603144a55f36763a94
YOON <50129701+yoondiver@users.noreply.github.com>
2023-12-05 23:11:55 +0900

    p1-42

--- programmers_lv1_42_K번째수.js
+++ programmers_lv1_42_K번째수.js
@@ -69,7 +69,7 @@ function solution(array, commands) {
   let answer = [];
   for (let i = 0; i < commands.length; i++) {
     let eachCommand = commands[i];
-    answer.push(slice.sort(a, b));
+    answer.push((slice.sort(a, b)=>a-b));
   }
   return answer;
 }
