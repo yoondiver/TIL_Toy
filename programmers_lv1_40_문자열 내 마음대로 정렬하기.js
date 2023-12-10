68d7fcff98b44091f6eb2da7b9f18972c25b3322
YOON <50129701+yoondiver@users.noreply.github.com>
2023-12-11 05:50:20 +0900

    p1-40

--- programmers_lv1_40_문자열 내 마음대로 정렬하기.js
+++ programmers_lv1_40_문자열 내 마음대로 정렬하기.js
@@ -54,3 +54,11 @@ function solution(strings, n) {
     return answer;
 }
 */
+
+function solution(strings, n) {
+  // strings 배열
+  // n 번째 문자열 비교
+  return strings.sort((s1, s2) =>
+    s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n])
+  );
+}
