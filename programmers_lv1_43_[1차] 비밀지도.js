7b3acfe6c09ae7ba93d816201bc0c28a103d4161
YOON <50129701+yoondiver@users.noreply.github.com>
2023-12-22 00:00:23 +0900

    p1-43

--- programmers_lv1_43_[1차] 비밀지도.js
+++ programmers_lv1_43_[1차] 비밀지도.js
@@ -20,13 +20,11 @@ arr1, arr2는 길이 n인 정수 배열로 주어진다.
 */
 
 function solution(n, arr1, arr2) {
-  var answer = [];
   return arr1.map((map1, i) => {
     map1 = map1.toString(2).padStart(n, "0");
     const map2 = arr2[i].toString(2).padStart(n, "0");
-
     return map1.split("").reduce((acc, cur, i) => {
-      return acc + (cur === "1" || map2[i] === "1" ? "#" : "");
+      return acc + (cur === "1" || map2[i] === "1" ? "#" : " ");
     }, "");
   });
 }
