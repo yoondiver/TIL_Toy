ad0a729383f688f534d30ed889b1d8bb772bbee9
YOON <50129701+yoondiver@users.noreply.github.com>
2024-02-04 21:37:51 +0900

    p1-44

--- programmers_lv1_44_푸드 파이트 대회.js
+++ programmers_lv1_44_푸드 파이트 대회.js
@@ -57,9 +57,9 @@ function solution(food) {
 
 function solution(food) {
   let res = "";
+
   for (let i = 1; i < food.length; i++) {
     res += String(i).repeat(Math.floor(food[i] / 2));
   }
-
   return res + "0" + [...res].reverse().join("");
 }
