c8d7f9d73ad37e295bc1caa642fc3e1f293312ea
YOON <50129701+yoondiver@users.noreply.github.com>
2024-03-20 23:35:08 +0900

    p1-45

--- programmers_lv1_45_두 개 뽑아서 더하기.js
+++ programmers_lv1_45_두 개 뽑아서 더하기.js
@@ -48,7 +48,7 @@ function solution(numbers) {
   var answer = [];
 
   for (let i = 0; i < numbers.length - 1; i++) {
-    for(let j=i+1; j<numbers){
+    for(let j=i+1; j<numbers.length){
 
     }
   }
