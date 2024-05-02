308ec7aab4e1d73901e20a62043da6f2ae468d40
YOON <50129701+yoondiver@users.noreply.github.com>
2024-05-02 23:57:23 +0900

    p1-46

--- programmers_lv1_46_[1차] 비밀지도.js
+++ programmers_lv1_46_[1차] 비밀지도.js
@@ -16,6 +16,7 @@ function solution(n, arr1, arr2) {
     map1.push(arr1[i].toString(2).padStart(n, 0));
     map2.push(arr2[i].toString(2).padStart(n, 0));
   }
+
   for (let i = 0; i < n; i++) {
     let tmp = "";
     for (let j = 0; j < n; j++) {
