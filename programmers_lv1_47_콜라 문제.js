245bb8759f59614bdd6034ef5849dadaf1fb365c
YOON <50129701+yoondiver@users.noreply.github.com>
2024-07-02 22:13:07 +0900

    p1-47

--- programmers_lv1_47_콜라 문제.js
+++ programmers_lv1_47_콜라 문제.js
@@ -52,6 +52,7 @@ solution = (a, b, n) => Math.floor(Math.max(n - b, 0) / (a - b)) * b;
 
 function solution(a, b, n) {
   let answer = 0;
+
   while (n >= a) {
     answer += parseInt(n / a) * b;
     n = parseInt(n / a) * b + n;
