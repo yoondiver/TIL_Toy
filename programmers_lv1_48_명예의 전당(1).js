33f172039ac21368ef3a0a8a8b09ff13787f61bb
YOON <50129701+yoondiver@users.noreply.github.com>
2024-07-15 23:39:53 +0900

    p1-48

--- programmers_lv1_48_명예의 전당(1).js
+++ programmers_lv1_48_명예의 전당(1).js
@@ -36,7 +36,7 @@ function solution(k, score) {
           return b - a;
         }),
         answer.push(n[i]))
-      : (n[k - 1]>= score[i])
+      : (n[k - 1]>= score[i]? n)
 
   return answer;
 }
