3891c2155295858e606144d7797f402946973518
YOON <50129701+yoondiver@users.noreply.github.com>
2024-08-05 20:21:56 +0900

    ㅔ1-48

--- programmers_lv1_48_명예의 전당(1).js
+++ programmers_lv1_48_명예의 전당(1).js
@@ -45,18 +45,18 @@ function solution(k, score) {
   }
   return answer;
 }
-
 function solution(k, score) {
   const stack = [];
-
   return score.reduce((a, c) => {
     if (stack.length < k) {
+      stack.push(c);
+      stack.sort((a, b) => a - b);
+    } else {
       stack.push(c);
       stack.sort((a, b) => a - b);
       stack.shift();
     }
     a.push(stack[0]);
-
     return a;
   }, []);
 }
