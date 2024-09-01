447af07080634c46e8b55361160fbfd9f8f22a62
YOON <50129701+yoondiver@users.noreply.github.com>
2024-09-02 01:36:30 +0900

    p1-49

--- programmers_lv1_49_추억 점수.js
+++ programmers_lv1_49_추억 점수.js
@@ -68,6 +68,6 @@ function solution(name, yearning, photo) {
 
 function solution(name, yearning, photo) {
   return photo.map((v) =>
-    v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0))
+    v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0), 0)
   );
 }
