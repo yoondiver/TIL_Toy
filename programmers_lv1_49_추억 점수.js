2e36886bdb1b6059b282a64e4550d0dbf903175a
YOON <50129701+yoondiver@users.noreply.github.com>
2024-09-15 17:05:28 +0900

    p1-49

--- programmers_lv1_49_추억 점수.js
+++ programmers_lv1_49_추억 점수.js
@@ -76,11 +76,10 @@ function solution(name, yearning, photo) {
 
 function solution(name, yearning, photo) {
   let obj = {};
-
   for (let i = 0; i < name.length; i++) {
     obj[name[i]] = yearning[i];
   }
   return photo.map((value) =>
-    value.map((v) => (obj[v] ? obj[v] : 0).reduce((acc, cur) => acc + cur, 0))
+    value.map((v) => (obj[v] ? obj[v] : 0)).reduce((acc, cur) => acc + cur, 0)
   );
 }
