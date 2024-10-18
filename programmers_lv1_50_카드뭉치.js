5ffbf82017a2b28a5844e945b6294011ada4b28a
YOON <50129701+yoondiver@users.noreply.github.com>
2024-10-18 23:16:49 +0900

    p1-50

--- programmers_lv1_50_카드뭉치.js
+++ programmers_lv1_50_카드뭉치.js
@@ -40,7 +40,7 @@ function solution(cards1, cards2, goal) {
   let stack = [];
   for (let i = 0; i < goal.length; i++) {
     if (goal[i] === cards1[0]) {
-      stack.push(cards1);
+      stack.push(cards1.shift);
     }
     return no;
   }
